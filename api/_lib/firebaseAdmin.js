24fa625776048ec123ecaf406afe4239f4c69925
prince143pp <alonebubby@gmail.com>
2026-05-01 09:56:02 +0530

    fix firebase init

--- api/_lib/firebaseAdmin.js
+++ api/_lib/firebaseAdmin.js
@@ -1,17 +1,14 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
-      })
-    });
-  } catch (e) {
-    console.error("Firebase Init Crash:", e);
-  }
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n') : undefined,
+    }),
+  });
 }
 
+export const db = admin.firestore();
 export default admin;
